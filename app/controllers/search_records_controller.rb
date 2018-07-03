8059a4c91af65a51e0289dafc831c4a2d8cdf9d6
Sudaraka Jayathilaka <sudarakayasindu@gmail.com>
2018-07-03 12:37:12 +0000

    Code cleanup and dynamic country in the address

--- app/controllers/search_records_controller.rb
+++ app/controllers/search_records_controller.rb
@@ -177,63 +177,56 @@ class SearchRecordsController < ApplicationController
     session[:viewed] ||= []
   end
 
-# implementation of the citation generator  
+# implementation of the citation generator
   def show_citation
     @page_number = params[:page_number].to_i
-    
+
     if params[:id].nil?
       redirect_to new_search_query_path
       return
     end
-    
+
     @search_record = SearchRecord.record_id(params[:id]).first
-    @searched_user_name = @search_record.transcript_names.first['first_name'] + " " + @search_record.transcript_names.first['last_name']
-    @viewed_date = Date.today.strftime("%e %b %Y")
-      
     @individual = @search_record.freecen_individual
     @dwelling = @individual.freecen_dwelling if @individual
-    
-    @is_family_head = false
-    @family_head_name = nil
-    #checks whether the head of the house is the same person searched for 
-    if @dwelling.freecen_individuals.asc(:sequence_in_household).first['forenames'].eql? @search_record.transcript_names.first['first_name'] 
-      @is_family_head = true
-    else
-      @family_head_name = @dwelling.freecen_individuals.asc(:sequence_in_household).first['forenames'] + " " + @dwelling.freecen_individuals.asc(:sequence_in_household).first['surname']
-    end
-    
     @cen_year = ' '
-    @cen_piece = ' '
     @cen_chapman_code = ' '
+
     if @dwelling && @dwelling.freecen_piece
-      @dwelling_offset = 0
-      @dwelling_number = @dwelling.dwelling_number
       if !params[:dwel].nil?
         @dwelling = @dwelling.freecen_piece.freecen_dwellings.where(_id: params[:dwel]).first
         if @dwelling.nil?
           redirect_to new_search_query_path
           return
         end
-        @dwelling_offset = @dwelling.dwelling_number - @dwelling_number
-        @dwelling_number = @dwelling.dwelling_number
       end
       @cen_year = @dwelling.freecen_piece.year
-      @cen_piece = @dwelling.freecen_piece.piece_number.to_s
       @cen_chapman_code = @dwelling.freecen_piece.chapman_code
-      prev_next_dwellings = @dwelling.prev_next_dwelling_ids
-      @cen_prev_dwelling = prev_next_dwellings[0]
-      @cen_next_dwelling = prev_next_dwellings[1]
-      @display_date = true
-      
       @dweling_values = @dwelling.dwelling_display_values(@cen_year,@cen_chapman_code)
-      @user_address = @dweling_values[11] + ", " +  @dweling_values[2] + ", " +  @dweling_values[1].slice(0..(@dweling_values[1].index(' ')-1)) 
+
+#   ------------------------ Fields required for citation generation ------------------------
+      @user_address = @dweling_values[11] + ", " +  @dweling_values[2] + ", " +  @dweling_values[1].slice(0..(@dweling_values[1].index(' ')-1)) + ", " + @search_record.place["country"]
 
       #census database description
       @census_database = "England and Wales Census, "
       if @search_record.place["country"].eql? "Scotland"
         @census_database = "Scotland Census, "
       end
-    @census_database += @cen_year
+      @census_database += @cen_year
+
+      @searched_user_name = @search_record.transcript_names.first['first_name'] + " " + @search_record.transcript_names.first['last_name']
+      @viewed_date = Date.today.strftime("%e %b %Y")
+      @is_family_head = false
+      @family_head_name = nil
+
+      #checks whether the head of the house is the same person searched for
+      if @dwelling.freecen_individuals.asc(:sequence_in_household).first['forenames'].eql? @search_record.transcript_names.first['first_name']
+        @is_family_head = true
+      else
+        @family_head_name = @dwelling.freecen_individuals.asc(:sequence_in_household).first['forenames'] + " " + @dwelling.freecen_individuals.asc(:sequence_in_household).first['surname']
+      end
+#   ------------------------ End of fields required for citation generation ------------------------
+
       render "_search_records_freecen_citation", :layout => false
       return
     end
@@ -250,7 +243,7 @@ class SearchRecordsController < ApplicationController
         @previous_record = @search_query.previous_record(params[:id])
         @next_record = @search_query.next_record(params[:id])
       end
-      
+
     rescue Mongoid::Errors::DocumentNotFound
       log_possible_host_change
       redirect_to new_search_query_path
@@ -261,7 +254,7 @@ class SearchRecordsController < ApplicationController
       redirect_to new_search_query_path
       return
     end
-    
+
     @display_date = true
     @entry.display_fields(@search_record) if @entry
     @annotations = Annotation.find(@search_record.annotation_ids) if @search_record.annotation_ids
@@ -269,5 +262,5 @@ class SearchRecordsController < ApplicationController
     @viewed_records << params[:id] unless @viewed_records.include?(params[:id])
     @search_result.update_attribute(:viewed_records, @viewed_records)
     render "show", :layout => false
-  end  
+  end
 end
